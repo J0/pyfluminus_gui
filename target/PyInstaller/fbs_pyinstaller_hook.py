623c2dc0c44d957fee3ed93f8001c319a78ffcf9
Lee Yi Jie Joel <lee.yi.jie.joel@gmail.com>
2020-07-21 00:35:39 +0800

    Restructure to support fbs

---
+++ target/PyInstaller/fbs_pyinstaller_hook.py
@@ -0,0 +1,3 @@
+import importlib
+module = importlib.import_module('fbs_runtime._frozen')
+module.BUILD_SETTINGS = {'app_name': 'pyfluminus_gui', 'author': 'Joel', 'version': '0.0.0', 'environment': 'local'}
